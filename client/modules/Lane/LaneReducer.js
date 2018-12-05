ec52fd23b23270b4d75b9c22955dd6a21c8cac0a
Kamil Szczypior <kszczypior1@gmail.com>
2018-12-05 23:14:27 +0100

    Add Lane client reducers, actions, component

--- client/modules/Lane/LaneReducer.js
+++ client/modules/Lane/LaneReducer.js
@@ -1,14 +1,45 @@
 // Import Actions
-import {  } from './LaneActions';
+import { CREATE_LANE, UPDATE_LANE, DELETE_LANE  } from './LaneActions';
+import { CREATE_NOTE, DELETE_NOTE } from '../Note/NoteActions';
+
 
 // Initial State
-const initialState = {};
+const initialState = [];
 
-const LaneReducer = (state = initialState, action) => {
+
+export default function lanes(state = initialState, action) {
   switch (action.type) {
+    case CREATE_LANE :
+      return [...state, action.lane];
+    case UPDATE_LANE :
+      return state.map(lane => {
+        if (lane.id === action.id) {
+          return Object.assign({}, lane, action.lane);
+        }
+        return lane;
+      });
+    case DELETE_LANE :
+      return state.filter(lane => lane.id !== action.laneId);
+    // this is update reducer for add note!
+    case CREATE_NOTE :
+      return state.map(lane => {
+        if (lane.id === action.laneId) {
+          const notes = [...lane.notes, action.note.id];
+          return { ...lane, notes };
+        }
+        return lane;
+      })
+    // this is update reducer for delete note!  kodilla quest !
+    case DELETE_NOTE :
+      return state.map(lane => {
+        if (lane.id === action.laneId ) {
+          return lane.notes.filter(note => note.id !== action.note.id);
+        }
+        return lane;
+      })
     default:
       return state;
-  }
-};
+    }
+}
 
-export default LaneReducer;
+
