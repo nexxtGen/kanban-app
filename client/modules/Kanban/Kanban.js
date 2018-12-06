e58cf1d092ed889a8471031c5f51d2f348107463
Kamil Szczypior <kszczypior1@gmail.com>
2018-12-06 10:57:21 +0100

    Add redux containers, some components

--- client/modules/Kanban/Kanban.js
+++ client/modules/Kanban/Kanban.js
@@ -1,27 +1,34 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
+import PropTypes from 'prop-types'
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import Lanes from '../Lane/Lanes';
+
+import { createLane } from '../Lane/LaneActions'; //import action creator to add new lane
 
 // Import Style
 import styles from './Kanban.css';
+import styles from '../Lane/Lane.css'
 
-class Kanban extends Component {
-  render() {
-    return (
-    );
-  }
+const Kanban = (props) => {
+  <div>
+    <button className={styles.AddLane} onClick={() => props.createLane({ name: 'New lane'}) }>Add lane</button>
+    <Lanes lanes={props.lanes} />
+  </div>
 }
+// Achtung! Wyjaśnienie i sama metoda w dalszej części kursu.
+Kanban.need = [() => { return fetchLanes(); }];
 
 const mapStateToProps = (state) => {
   return {};
 };
 
-const mapDispatchToProps = (dispatch) => {
-  return {};
+const mapDispatchToProps =  {
+  createLane,
 };
 
 Kanban.propTypes = {
+  lanes: PropTypes.array,
+  createLane: PropTypes.func,
 };
 
 export default connect(
---
+++ client/modules/Note/Notes.js
@@ -0,0 +1,24 @@
+import React, { PropTypes } from 'react';
+import Note from './Note';
+import styles from './Note.css';
+
+const Notes = ({ notes } ) => {
+    return (
+        <ul className="notes">
+            {notes.map((note) =>
+                <Note
+                    id={note.id}
+                    key={note.id}
+                >
+                    {note.task}
+                </Note>                   
+            )}
+        </ul>
+    );
+};
+
+Notes.propTypes = {
+    notes: PropTypes.array
+}
+
+export default Notes;
