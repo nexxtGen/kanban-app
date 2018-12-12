c758d7e6bde7807456721ec86c24b560414fef0e
Kamil Szczypior <kszczypior1@gmail.com>
2018-12-12 23:58:17 +0100

    implements Note d&d

--- client/modules/Note/Note.js
+++ client/modules/Note/Note.js
@@ -1,15 +1,67 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Note.css';
+// Drag &drop
+import {DragSource, DropTarget} from 'react-dnd';
+import {compose} from 'redux';
+import ItemTypes from '../Kanban/itemTypes';
 
-const Note = (props) => {
-  return (
-    <li className={'styles.Note'}>{props.children}</li>
-  );
+
+// Changes -d&d
+class Note extends React.Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+  }
+  render() {
+    const {connectDragSource, isDragging,
+      editing, children} = this.props;
+
+    // jeśli edytujemy to przepuszczamy komponent (uniemożliwiamy tym samym przeciąganie komponentu edytowanego)
+    const dragSource = editing ? a => a : connectDragSource;
+
+    return dragSource(
+      <li className={styles.Note}
+        style={{
+        opacity: isDragging ? 0 : 1
+      }} >{children}</li>
+    );
+  }
 }
+
+const noteSource = {
+  beginDrag(props) {
+    return {
+      id: props.id,
+      laneId: props.laneId,
+    };
+  },
+  isDragging(props, monitor) {
+    return props.id === monitor.getItem().id;
+  }
+};
+
+const noteTarget = {
+  hover(targetProps, monitor) {
+    const sourceProps = monitor.getItem();
+
+    if (targetProps.id !== sourceProps.id) {
+      targetProps.moveWithinLane(targetProps.laneId, targetProps.id, sourceProps.id);
+    }
+  }
+};
 
 Note.propTypes = {
   children: PropTypes.any,
 };
 
-export default Note;
+//d&d
+export default compose(
+  DragSource(ItemTypes.NOTE, noteSource, (connect, monitor) => ({
+    connectDragSource: connect.dragSource(),
+    isDragging: monitor.isDragging()
+  })),
+  DropTarget(ItemTypes.NOTE, noteTarget, (connect) => ({
+    connectDropTarget: connect.dropTarget()
+  }))
+)(Note);
